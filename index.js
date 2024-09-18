eb4e1a623663d9432c89ce0bb815f0213b333718
John <jwbarand@gmail.com>
2024-09-18 12:50:38 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+var num1 = 31;
+var num2 = 2;
+var multiply = (num1 * num2);
+multiply;
 
-// Write your code here
+var random = Math.floor(Math.random()*10);
+
+var num3 = 12;
+var num4 = 3;
+var mod = (num3 / num4);
+mod;
+
+var max = Math.max(1,2,3,4,5,6,18,20);
+max;
